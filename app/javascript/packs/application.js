3cdf868f3f42ca4ea62e86f290da60ddb4272b8e
Tawasi51 <koutyoku1220satori@gmail.com>
2020-09-23 19:23:11 +0900

    変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../ingredient")
-require("../recipe")
 require('jquery')
 
 // Uncomment to copy all static images under ../images to the output folder and reference
